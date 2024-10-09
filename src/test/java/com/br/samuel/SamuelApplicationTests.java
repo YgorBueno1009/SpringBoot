086afa2ed029defd6d94db8a76d0b2a4b05a689e
YgorBueno1009 <143885243+YgorBueno1009@users.noreply.github.com>
2024-10-09 15:19:21 -0300

    First commit

---
+++ src/test/java/com/br/samuel/SamuelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.samuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
